6ce6bb7161d1731c7ec336d57f196a2f7151cdb4
hexap <47399791+closedAi@users.noreply.github.com>
2021-04-28 13:52:42 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# seps-api-face-recognition
+API created by me for our senior project.
---
+++ main.py
@@ -0,0 +1,81 @@
+import flask
+from flask import request, jsonify
+from PIL import Image, ImageDraw, ImageEnhance
+import requests
+import numpy as np
+import urllib
+import cv2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+
+model = load_model("mask_detector.model")
+prototxtPath = "deploy.prototxt"
+weightsPath = "res10_300x300_ssd_iter_140000.caffemodel"
+net = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<p>The mask recognition API for Social Event Photo Event project, contact: taner.durmaz.cs@gmail.com </p>"
+
+
+@app.route('/find/<path:url>', methods=['GET'])
+def find(url):
+
+    #im = Image.open(requests.get(url, stream=True).raw)
+    #im = im.convert('RGB')
+    #unknown_image = np.array(im)
+
+image = cv2.imread("obama.jpg")
+    (h, w) = image.shape[:2]
+
+    # construct a blob from the image
+    blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
+                                 (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the face detections
+    print("[INFO] computing face detections...")
+    net.setInput(blob)
+    detections = net.forward()
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > 0.5:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+            face = image[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+            face = np.expand_dims(face, axis=0)
+
+            # pass the face through the model to determine if the face
+            # has a mask or not
+            (mask, withoutMask) = model.predict(face)[0]
+
+            # determine the class label and color we'll use to draw
+            # the bounding box and text
+            label = "Mask" if mask > withoutMask else "No Mask"
+    return label
